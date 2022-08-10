7b191abc51e9cd292ebafc28a87a158e526ac5bc
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-08-10 21:59:41 +0300

    Update YourCartPage.java

--- src/main/java/pages/YourCartPage.java
+++ src/main/java/pages/YourCartPage.java
@@ -1,7 +1,19 @@
 package pages;
 
-public class YourCartPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class YourCartPage extends BasePage {
 
+    @FindBy(className = "title")
+    WebElement title;
 
+    public YourCartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getTitle() {
+        return getElementText(title);
+    }
 }
