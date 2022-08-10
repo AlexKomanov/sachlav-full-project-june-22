7a86b8e37de8276b209dd0abd6c3e7601737fcdd
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-08-10 21:34:25 +0300

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,33 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+    @FindBy(css = "[id='password']")
+    private WebElement passwordInput;
+    @FindBy(css = "[id=\"login-button\"]")
+    private WebElement loginButton;
+    @FindBy(css = "[data-test=\"error\"]")
+    private WebElement errorMessage;
+
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void loginToApp(String username, String password){
+        fillText(usernameInput, username);
+        fillText(passwordInput, password);
+        clickElement(loginButton);
+    }
+
+    public String getErrorMessage() {
+        return getElementText(errorMessage);
+    }
+
 }
