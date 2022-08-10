aa76fb793716d0889afd58261713c433b0eae2c4
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-08-10 21:33:49 +0300

    Create BaseTest.java

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait webDriverWait;
+
+    @BeforeSuite
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void init(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriverWait = new WebDriverWait(driver, 30);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("Before Test Block");
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("After Test Block");
+    }
+}
