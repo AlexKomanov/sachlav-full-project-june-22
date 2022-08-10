d71cff88bfef8ff1bc346eeba95a4971cd73b844
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-08-10 21:33:56 +0300

    Create Credentials.java

---
+++ src/main/java/helpers/Credentials.java
@@ -0,0 +1,9 @@
+package helpers;
+
+public class Credentials {
+
+    public static final String STANDARD_USER = "standard_user";
+    public static final String LOCKED_OUT_USER = "locked_out_user";
+
+    public static final String SECRET_SAUCE_PASSWORD = "secret_sauce";
+}
