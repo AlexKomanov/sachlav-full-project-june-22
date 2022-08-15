eb95ce4ba73a0990f5de1389f0f2ad88e044fb71
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-08-15 21:35:55 +0300

    Commit with all required code

--- src/test/java/tests/logintests/failedTests/FailedTests.java
+++ src/test/java/tests/logintests/failedTests/FailedTests.java
@@ -1,4 +1,22 @@
 package tests.logintests.failedTests;
 
-public class FailedTests {
+import helpers.Credentials;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import tests.BaseTest;
+
+public class FailedTests extends BaseTest {
+
+    @Test
+    public void failedTest_01() {
+        LoginPage loginPage = new LoginPage(driver);
+
+
+        loginPage.loginToApp(Credentials.LOCKED_OUT_USER, Credentials.SECRET_SAUCE_PASSWORD);
+
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Sorry, this user has been locked out.");
+    }
+
+
 }
--- src/test/java/tests/logintests/successTest/SuccessTests.java
+++ src/test/java/tests/logintests/successTest/SuccessTests.java
@@ -1,4 +1,22 @@
 package tests.logintests.successTest;
 
-public class SuccessTests {
+import helpers.Credentials;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProductsPage;
+import tests.BaseTest;
+
+public class SuccessTests extends BaseTest {
+
+    @Test
+    public void successTest_01() {
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productsPage = new ProductsPage(driver);
+
+        loginPage.loginToApp(Credentials.STANDARD_USER, Credentials.SECRET_SAUCE_PASSWORD);
+
+        String productsPageTitle = productsPage.getTitle();
+        Assert.assertEquals(productsPageTitle, "PRODUCTS");
+    }
 }
